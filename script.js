c2372a7d4a5ccf84ee0d867a67d5eea7e2fb4f2b
Maria <mmhpopoca@gmail.com>
2018-07-24 15:33:06 -0500

    <commit message>

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	
+	console.log("BE Creative");
+	$("h1").css("text-align", "center");
+});
+$("button").click(function() {
+	console.log("noun") ;
+var noun = $("#noun").val();
+	console.log(noun);
+	});
